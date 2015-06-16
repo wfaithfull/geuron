40c6b064776e5000cddab7793c790c23dc4cc842
Will Faithfull <will@sourcepulp.com>
2015-06-16 09:52:04 +0100

    reduced test to stub. it has nothing to test yet anyway

--- src/test/java/me/faithfull/geuron/test/ImageServiceTest.java
+++ src/test/java/me/faithfull/geuron/test/ImageServiceTest.java
@@ -1,21 +1,6 @@
 package me.faithfull.geuron.test;
 
-import me.faithfull.geuron.imagesearch.ImageService;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@Component
 public class ImageServiceTest {
 
-	@Autowired private ImageService imageService;
-	
-	@Test
-	public void getThumbs() {
-		imageService.getImages("monkey", 0);
-	}
 }
