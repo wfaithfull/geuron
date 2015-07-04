07e9e4e82392865f1f4101063794d9a79239ab26
wfaithfull <willfaithfull89@gmail.com>
2015-07-04 22:45:33 +0100

    added basic image selection

--- src/main/resources/static/js/geuron.js
+++ src/main/resources/static/js/geuron.js
@@ -1,30 +1,50 @@
 $(function() {
-	$('#searchTerms').submit(function(args){
+	$('#searchTerms').submit(function(args) {
 		$('#grid').empty();
 		getThumbs($('#searchTermsInput').val(), 0, 8);
+		$("#grid").on('click', 'img.thumb', function() {
+			selectThumb(this);
+		});
 		return false;
 	})
 })
 
+var chosenThumbs = [];
+
 function getThumbs(searchTerms, start, batchSize) {
-	
+
 	console.log("api request... " + searchTerms);
 	var max = 50;
 	$.ajax({
-		type: 'GET',
-		url: window.location.href + "/images/" + searchTerms.toString() + "/" + start.toString(),
-		success: function(result) {
+		type : 'GET',
+		url : window.location.href + "/images/" + searchTerms.toString() + "/"
+				+ start.toString(),
+		success : function(result) {
 			console.log(result);
-			
-			for(i=0;i<result.length;i++) {
+
+			for (i = 0; i < result.length; i++) {
 				thumb = result[i];
-				$("#grid").append($("<img>", { src : thumb.tbUrl }));
+				$("#grid").append($("<img>", {
+					src : thumb.tbUrl,
+					id : thumb.imageId,
+					class : "thumb"
+				}));
+
 			}
-			if(start < max) {
+
+			if (start < max) {
 				start = start + batchSize;
 				getThumbs(searchTerms, start, batchSize);
 			}
 		}
 	})
-	
+
+}
+
+function selectThumb(element) {
+	if ($(element).hasClass("selected-thumb")) {
+		$(element).removeClass("selected-thumb")
+	} else {
+		$(element).addClass("selected-thumb");
+	}
 }
