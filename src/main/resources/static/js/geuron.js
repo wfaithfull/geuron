0816cd63a6b6aed73b62220fd82348cad6bc1b87
wfaithfull <willfaithfull89@gmail.com>
2015-07-05 12:23:03 +0100

    add custom photoset grid

--- src/main/resources/static/js/geuron.js
+++ src/main/resources/static/js/geuron.js
@@ -1,4 +1,22 @@
 $(function() {
+	
+	$('.photoset-grid-custom').photosetGrid({
+		  // Set the gutter between columns and rows
+		  gutter: '5px',
+		  // Manually set the grid layout
+		  layout: '21',
+		  // Wrap the images in links
+		  highresLinks: true,
+		  // Asign a common rel attribute
+		  rel: 'print-gallery',
+
+		  onInit: function(){},
+		  onComplete: function(){
+		    // Show the grid after it renders
+		    $('.photoset-grid-custom').attr('style', '');
+		  }
+		});
+	
 	$('#searchTerms').submit(function(args) {
 		$('#grid').empty();
 		getThumbs($('#searchTermsInput').val(), 0, 8);
@@ -11,6 +29,10 @@ $(function() {
 
 var chosenThumbs = [];
 
+/*
+ * Calls the geuron API which retrieves thumbnails from the
+ * concrete image search service.
+ */
 function getThumbs(searchTerms, start, batchSize) {
 
 	console.log("api request... " + searchTerms);
@@ -41,6 +63,10 @@ function getThumbs(searchTerms, start, batchSize) {
 
 }
 
+/*
+ * When passed an <img> tag, visually selects it for the user.
+ * TODO: Add / remove chosen thumb to / from collection.
+ */
 function selectThumb(element) {
 	if ($(element).hasClass("selected-thumb")) {
 		$(element).removeClass("selected-thumb")
